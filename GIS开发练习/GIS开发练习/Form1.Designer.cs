851f0c09fe61c8f89fd7075696ae8e4fe75d32c4
3425205233 <3425205233@qq.com>
2020-07-23 13:26:10 +0800

    gis开发

--- GIS开发练习/GIS开发练习/Form1.Designer.cs
+++ GIS开发练习/GIS开发练习/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.axToolbarControl1 = new ESRI.ArcGIS.Controls.AxToolbarControl();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
@@ -38,7 +41,9 @@
             this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.缩放至图层ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -47,23 +52,58 @@
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
             this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.axLicenseControl1);
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.axToolbarControl1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(507, 79);
+            this.panel1.Size = new System.Drawing.Size(676, 99);
             this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "合并算法";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(555, 47);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "调用工具";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(48, 34);
+            this.button1.Location = new System.Drawing.Point(64, 42);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 29);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -73,15 +113,17 @@
             // 
             this.axToolbarControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.axToolbarControl1.Location = new System.Drawing.Point(0, 0);
+            this.axToolbarControl1.Margin = new System.Windows.Forms.Padding(4);
             this.axToolbarControl1.Name = "axToolbarControl1";
             this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
-            this.axToolbarControl1.Size = new System.Drawing.Size(507, 28);
+            this.axToolbarControl1.Size = new System.Drawing.Size(676, 28);
             this.axToolbarControl1.TabIndex = 0;
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 79);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 99);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -90,18 +132,21 @@
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
             this.splitContainer1.Panel2.Controls.Add(this.axMapControl1);
-            this.splitContainer1.Size = new System.Drawing.Size(507, 224);
-            this.splitContainer1.SplitterDistance = 169;
+            this.splitContainer1.Size = new System.Drawing.Size(676, 280);
+            this.splitContainer1.SplitterDistance = 225;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 1;
             // 
             // axTOCControl1
             // 
             this.axTOCControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.axTOCControl1.Location = new System.Drawing.Point(0, 0);
+            this.axTOCControl1.Margin = new System.Windows.Forms.Padding(4);
             this.axTOCControl1.Name = "axTOCControl1";
             this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
-            this.axTOCControl1.Size = new System.Drawing.Size(169, 224);
+            this.axTOCControl1.Size = new System.Drawing.Size(225, 280);
             this.axTOCControl1.TabIndex = 0;
             this.axTOCControl1.OnMouseDown += new ESRI.ArcGIS.Controls.ITOCControlEvents_Ax_OnMouseDownEventHandler(this.axTOCControl1_OnMouseDown);
             // 
@@ -109,38 +154,51 @@
             // 
             this.axMapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.axMapControl1.Location = new System.Drawing.Point(0, 0);
+            this.axMapControl1.Margin = new System.Windows.Forms.Padding(4);
             this.axMapControl1.Name = "axMapControl1";
             this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
-            this.axMapControl1.Size = new System.Drawing.Size(334, 224);
+            this.axMapControl1.Size = new System.Drawing.Size(446, 280);
             this.axMapControl1.TabIndex = 0;
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.缩放至图层ToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 26);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 28);
             this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
             // 
             // 缩放至图层ToolStripMenuItem
             // 
             this.缩放至图层ToolStripMenuItem.Name = "缩放至图层ToolStripMenuItem";
-            this.缩放至图层ToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.缩放至图层ToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
             this.缩放至图层ToolStripMenuItem.Text = "缩放至图层";
             this.缩放至图层ToolStripMenuItem.Click += new System.EventHandler(this.缩放至图层ToolStripMenuItem_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 7);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(391, 261);
+            this.dataGridView1.TabIndex = 1;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(507, 303);
+            this.ClientSize = new System.Drawing.Size(676, 379);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).EndInit();
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
@@ -149,6 +207,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
             this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -163,7 +222,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 缩放至图层ToolStripMenuItem;
-
+        private System.Windows.Forms.Button button2;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- GIS开发练习/GIS开发练习/Form1.cs
+++ GIS开发练习/GIS开发练习/Form1.cs
@@ -9,6 +9,11 @@ using System.Windows.Forms;
 using ESRI.ArcGIS.SystemUI;
 using ESRI.ArcGIS.Controls;
 using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geoprocessing;
+using ESRI.ArcGIS.esriSystem;
+using System.Data.OleDb;
+
+
 
 namespace GIS开发练习
 {
@@ -72,5 +77,385 @@ namespace GIS开发练习
             }
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            object sev = null;      //标记处理的地理信息
+
+            IGeoProcessor2 gp = new GeoProcessorClass();
+            gp.OverwriteOutput = true;      //设置gp.OverwriteOutput是指可以用一个输出执行多次工具
+            gp.AddToolbox(@"C:\Users\Administrator\Desktop\SlopExtract2\Process_Vital\ImportantFile\arcgis模型\工具箱.tbx");
+
+
+            IGeoProcessorResult result = new GeoProcessorResultClass();
+
+
+            //设置参数
+            IVariantArray parameters = new VarArrayClass();      //using ESRI.ArcGIS.esriSystem;引用
+            
+            try
+            {
+                parameters.Add(@"C:\Users\Administrator\Desktop\SlopExtract2\Data\Dem8mYX_CDG.tif");
+                parameters.Add("");
+                parameters.Add(@"C:\Users\Administrator\Desktop\SlopExtract\Process\TEST_0719\resutlt\abc");
+
+                 gp.Execute("模型", parameters, null);
+                MessageBox.Show( gp.GetMessages(ref sev));
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+
+            
+
+
+
+
+
+        }
+
+        /// <summary>
+        /// 合并算法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            DataSet dataSet = ExcelToDS(@"C:\Users\Administrator\Desktop\SlopExtract\Process\TEST_0715\table_邻近关系表.xls");
+
+            DataTable t = dataSet.Tables[0];
+            string a = t.Rows[0][0].ToString();
+
+            dataGridView1.DataSource = t;
+
+
+            DataTable comElement= GetCombinedElement();
+
+            DataSet ds = new DataSet();
+            ds.Tables.Add(comElement);
+
+            dataGridView1.DataSource = comElement;
+
+
+
+      
+
+
+        }
+
+
+
+        /// <summary>
+        /// 需要合并的邻近表记录（包括相邻面要素的ID）
+        /// </summary>
+        public struct MergeRecord
+        {
+            public int id1;
+            public int id2;
+
+        };
+
+
+
+        /// <summary>
+        /// 获取合并面要素集合（返回合并要素ID列表）
+        /// </summary>
+        public DataTable GetCombinedElement()
+        {
+            //定义主要变量
+            List<List<int>> CombinedList = new List<List<int>>();       //合并列表
+            List<MergeRecord> mRecords = new List<MergeRecord>();           //筛选出需要合并的相邻对象ID集合
+            DataTable table_surface;        //面要素属性表
+            DataTable table_near;               ///邻近表
+            Double limit = 5;               //限差
+
+
+            //获取要素属性表和邻近表
+            DataSet dataSet = ExcelToDS(@"C:\Users\Administrator\Desktop\SlopExtract\Process\TEST_0715\table_邻近关系表.xls");
+            table_near = dataSet.Tables[0];
+
+            dataSet = ExcelToDS(@"C:\Users\Administrator\Desktop\SlopExtract\Process\TEST_0715\table_坡体单元属性表_r.xls");
+            table_surface = dataSet.Tables[0];
+
+
+            //邻近表中，提取所有满足限定条件（f(坡度,坡向)<limit）的记录
+            foreach(DataRow row in table_near.Rows )
+            {
+                MergeRecord m;
+                m.id1 = Int32.Parse(row[1].ToString());     //获取邻近表中一条记录的两个对象ID
+                m.id2 = Int32.Parse(row[2].ToString());
+
+                if (table_surface.Rows[m.id1][4].ToString() == "" || table_surface.Rows[m.id2][4].ToString() == "")       //当存在未定义值的记录是，跳过
+                    continue;
+                
+
+                Double slop1 =Double.Parse( table_surface.Rows[m.id1][4].ToString());           //提取ID对应要素的坡度、坡向
+                Double slop2 = Double.Parse(table_surface.Rows[m.id2][4].ToString());
+                Double aspect1 = Double.Parse(table_surface.Rows[m.id1][5].ToString());
+                Double aspect2 = Double.Parse(table_surface.Rows[m.id2][5].ToString());
+
+
+                Double scale_factor = Math.Sqrt((slop1 - slop2) * (slop1 - slop2) + (aspect1 - aspect2) * (aspect1 - aspect2));     //计算尺度因子
+
+                if(scale_factor<limit)          //当尺度因子小于限差
+                {
+                    if(m.id1>m.id2)     //保持 id2>id1
+                    {
+                        int id_int = m.id2;
+                        m.id2 = m.id1;
+                        m.id1 = id_int;
+                    }
+
+                    bool isExist = false;           //判断m在mRecords中是否存在
+                    foreach(MergeRecord merge in mRecords)
+                    {
+                        if (m.id1 == merge.id1 && m.id2 == merge.id2)
+                            isExist = true;
+
+                    }
+
+                    if(!isExist)            //当不存在时，增加记录
+                    {
+                        mRecords.Add(m);
+                    }
+
+
+                }
+
+
+            }
+
+
+
+
+
+            //递归算法，将满足条件的邻近表转换为合并要素ID列表
+            List<int> element_ids = new List<int>();        //获取所有id1对象id集合
+            foreach(MergeRecord m in mRecords)
+            {
+                if (element_ids.Count == 0)
+                    element_ids.Add(m.id1);
+                else if (m.id1 != element_ids[element_ids.Count - 1])
+                    element_ids.Add(m.id1);
+
+            }
+
+
+            foreach(int id in element_ids)      //遍历element_ids中所有对象
+            {
+                bool isExit = false;
+
+                foreach(List<int> i in CombinedList)        //查找对象是否存在
+                {
+                    if (isExit)
+                        break;
+
+                    foreach(int j in i)
+                    {
+                        if(id==j)
+                        {
+                            isExit = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (isExit)
+                    continue;
+
+
+                List<int> ids = new List<int>();        //合并对象集合
+                GetList(id, ref ids, mRecords);         //获取对象
+                CombinedList.Add(ids);
+            }
+
+
+            //转化成table中的列
+
+            DataTable table = new DataTable();
+            table.Columns.Add("合并");
+
+
+            foreach(DataRow row  in table_surface.Rows)     //赋值id号的一列
+            {
+                DataRow r = table.NewRow();
+                r[0] = row[0];
+                table.Rows.Add(r);
+            }
+
+            foreach(List<int> elements in CombinedList)         //根据对象列表赋值
+            {
+                int value = elements[0];
+
+                foreach(int id in elements)
+                {
+                    
+                    table.Rows[id].BeginEdit();         //table赋值技巧
+                    table.Rows[id][0] = value;
+                    table.Rows[id].EndEdit();
+
+                }
+
+            }
+
+
+            return table;
+        }
+
+
+
+        public void GetList (int id, ref List<int> ids,List<MergeRecord> mRecords)
+        {
+            if (!IsIn(id,ids)  )       //加自身
+                ids.Add(id);
+
+            foreach(MergeRecord r in mRecords)      //获取该对象所有相邻对象
+            {
+                if (id == r.id1&&!IsIn(r.id2,ids))
+                {
+                    ids.Add(r.id2);
+                    GetList(r.id2, ref ids, mRecords);
+                }
+                    
+                if(id==r.id2&&!IsIn(r.id1,ids))
+                {
+                    ids.Add(r.id1);
+                    GetList(r.id1, ref ids, mRecords);
+                }
+
+
+            }
+
+
+
+        }
+
+        public bool IsIn(int id,List<int> ids)
+        {
+            bool isin = false;
+
+            foreach(int i in ids)
+            {
+                if (i == id)
+                    isin = true;
+            }
+
+            return isin;
+        }
+  
+             
+
+        /// <summary>
+        /// 根据值获取索引(未使用)
+        /// </summary>
+        /// <returns></returns>
+        public int GetIndexByValue(int value,DataColumn col)
+        {
+            int index=-1;
+            
+
+            return index;
+
+        }
+
+
+
+
+        /// <summary>
+        /// 读取excle表格
+        /// </summary>
+        /// <param name="Path"></param>
+        /// <returns></returns>
+        public DataSet ExcelToDS(string Path)
+        {
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Path + ";" + "Extended Properties=Excel 8.0;";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            string strExcel = "";
+            OleDbDataAdapter myCommand = null;
+            DataSet ds = null;
+
+            //获取表格的名称
+            DataTable schemaTable = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, null);
+            string tableName = schemaTable.Rows[0][2].ToString().Trim();
+
+
+            strExcel = "select * from ["+tableName+"]";
+            myCommand = new OleDbDataAdapter(strExcel, strConn);
+            ds = new DataSet();
+            myCommand.Fill(ds, "table1");
+            return ds;
+        }
+
+
+        /// <summary>
+        /// 写入Excel（未测试）
+        /// </summary>
+        /// <param name="Path"></param>
+        /// <param name="oldds"></param>
+        public void DSToExcel(string Path, DataSet oldds)
+        {
+            //先得到汇总EXCEL的DataSet 主要目的是获得EXCEL在DataSet中的结构 
+            string strCon = " Provider = Microsoft.Jet.OLEDB.4.0 ; Data Source =" + Path + ";Extended Properties=Excel 8.0";
+            OleDbConnection myConn = new OleDbConnection(strCon);
+            string strCom = "select * from [Sheet1$]";
+            myConn.Open();
+            OleDbDataAdapter myCommand = new OleDbDataAdapter(strCom, myConn);
+            System.Data.OleDb.OleDbCommandBuilder builder = new OleDbCommandBuilder(myCommand);
+            //QuotePrefix和QuoteSuffix主要是对builder生成InsertComment命令时使用。 
+            builder.QuotePrefix = "[";     //获取insert语句中保留字符（起始位置） 
+            builder.QuoteSuffix = "]"; //获取insert语句中保留字符（结束位置） 
+            DataSet newds = new DataSet();
+            myCommand.Fill(newds, "Table1");
+            for (int i = 0; i < oldds.Tables[0].Rows.Count; i++)
+            {
+                //在这里不能使用ImportRow方法将一行导入到news中，因为ImportRow将保留原来DataRow的所有设置(DataRowState状态不变)。在使用ImportRow后newds内有值，但不能更新到Excel中因为所有导入行的DataRowState != Added
+
+                DataRow nrow =newds.Tables["Table1"].NewRow();
+                for (int j = 0; j < newds.Tables[0].Columns.Count; j++)
+                {
+                    nrow[j] = oldds.Tables[0].Rows[i][j];
+                }
+                newds.Tables["Table1"].Rows.Add(nrow);
+            }
+            myCommand.Update(newds, "Table1");
+            myConn.Close();
+        }
+
+
+        /// <summary>
+        /// 读取shp文件
+        /// </summary>
+        /// <returns></returns>
+        public string[] OpenShapeFile()
+        {
+            string[] ShpFile = new string[2];
+            OpenFileDialog OpenShpFile = new OpenFileDialog();
+            OpenShpFile.Title = "打开Shape文件";
+            OpenShpFile.InitialDirectory = "E:";
+            OpenShpFile.Filter = "Shape文件(*.shp)|*.shp";
+
+            if (OpenShpFile.ShowDialog() == DialogResult.OK)
+            {
+                string ShapPath = OpenShpFile.FileName;
+                //利用"\\"将文件路径分成两部分
+                int Position = ShapPath.LastIndexOf("\\");
+
+                string FilePath = ShapPath.Substring(0, Position);
+                string ShpName = ShapPath.Substring(Position + 1);
+                ShpFile[0] = FilePath;
+
+                ShpFile[1] = ShpName;
+
+            }
+            return ShpFile;
+        }
+
+
+
     }
 }
